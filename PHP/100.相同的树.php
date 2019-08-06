e67229ad323721ff6b0f283e23578ec761c3b5fd
max.hua <max.hua@tousforfun.cn>
2019-08-06 16:39:46 +0800

    add the 11， 38 ，100

---
+++ PHP/100.相同的树.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Max
+ * Date: 2019/7/28
+ * Time: 15:30
+ */
+
+
+include_once "Common.php";
+
+
+/**
+ * 给定两个二叉树，编写一个函数来检验它们是否相同。
+ *
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ *
+ * 示例 1:
+ *
+ * 输入:       1         1
+ * ⁠         / \       / \
+ * ⁠        2   3     2   3
+ *
+ * ⁠       [1,2,3],   [1,2,3]
+ *
+ * 输出: true
+ *
+ * 示例 2:
+ *
+ * 输入:      1          1
+ * ⁠         /           \
+ * ⁠        2             2
+ *
+ * ⁠       [1,2],     [1,null,2]
+ *
+ * 输出: false
+ *
+ *
+ * 示例 3:
+ *
+ * 输入:       1         1
+ * ⁠         / \       / \
+ * ⁠        2   1     1   2
+ *
+ * ⁠       [1,2,1],   [1,1,2]
+ *
+ * 输出: false
+ *
+ */
+
+
+//Definition for a binary tree node.
+class TreeNode
+{
+    public $val   = null;
+    public $left  = null;
+    public $right = null;
+
+    function __construct($value)
+    {
+        $this->val = $value;
+    }
+}
+
+class Solution
+{
+
+    /**
+     * @param TreeNode $p
+     * @param TreeNode $q
+     * @return Boolean
+     */
+    function isSameTree($p, $q)
+    {
+        if(empty($p) && empty($q)){
+            return true;
+        }elseif (!empty($p) && !empty($q) && $p->val == $q->val){
+            return $this->isSameTree($p->left, $q->left) && $this->isSameTree($p->right, $q->right);
+        }else {
+            return false;
+        }
+    }
+}
